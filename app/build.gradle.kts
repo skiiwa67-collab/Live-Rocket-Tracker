db2048be3b757c5213ad4dd0dade1297d6317aa0
Ada <ada@local>
2026-09-09 12:41:06 -0500

    stamp 91: bump 91/1.0.81 (LRT91-STARSHIP)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.liverockettracker"
         minSdk = 24
         targetSdk = 35
-        versionCode = 90
-        versionName = "1.0.80"
+        versionCode = 91
+        versionName = "1.0.81"
     }
 
     buildTypes {
