7070cf23982880147c348cdffa514aace67cd367
Ada <ada@local>
2026-09-09 12:52:11 -0500

    stamp 92: bump 92/1.0.82 (LRT92-FULL)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,4 @@
-﻿plugins {
+plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
@@ -11,8 +11,8 @@ android {
         applicationId = "com.liverockettracker"
         minSdk = 24
         targetSdk = 35
-        versionCode = 91
-        versionName = "1.0.81"
+        versionCode = 92
+        versionName = "1.0.82"
     }
 
     buildTypes {
